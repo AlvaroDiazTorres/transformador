6fb4830f5aa8d553618f84f20b3ab5b38ac4f11e
AlvaroDiazTorres <adtclase@gmail.com>
2022-04-25 10:51:47 +0200

    first commit

---
+++ src/main/java/com/example/transformador/TransformadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.transformador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transformador/TransformadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transformador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
